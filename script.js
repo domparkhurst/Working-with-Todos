9746c0eecd6128abae826a5cdf35c5e553f285a5
Dominic <22148988+domparkhurst@users.noreply.github.com>
2019-07-17 16:30:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,26 @@
 var todoData = [];
 
 function newTodo() {
-    // get input value to array
+    // get input value and send it to the array
     var todoInput = document.getElementById("todo-input");
     todoData.push(todoInput.value);
     console.log(todoData);
     
-    // create list item and append value
+    // create a list item which appends input value
     var todoItems = document.getElementById("todo-items");
     var listItem = document.createElement("LI");
     var listItemTxt = document.createTextNode(todoInput.value);
     todoItems.appendChild(listItem);
-    listItem.appendChild(listItemTxt);
-}
 
-console.log(todoData);
+    // create a span element to hold text
+    var spanElem = document.createElement("SPAN");
+    listItem.appendChild(spanElem);
+    spanElem.appendChild(listItemTxt);
+
+    // create an input button to remove an element
+    // the input will be compatible with a form in the future
+    var inputDelElem = document.createElement("INPUT");
+    inputDelElem.setAttribute("type", "button");
+    inputDelElem.setAttribute("value", "X");
+    spanElem.appendChild(inputDelElem);
+}
