a1aa0e1bf7b8d538c7fd7d6192f4b0db0b95b527
Dominic <22148988+domparkhurst@users.noreply.github.com>
2019-07-20 18:49:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,37 @@
+// store todos
 var todoData = [];
 
 function newTodo() {
-    var todoInput = document.getElementById("todo-input");
-    todoData.push(todoInput.value);
+    var inputText = document.getElementById("todo-input");
+    todoData.push(inputText.value);
     
-    var todoItems = document.getElementById("todo-items");
+    var ulElem = document.getElementById("todo-items");
     var liElem = document.createElement("LI");
-    var liElemTxt = document.createTextNode(todoInput.value);
-    todoItems.appendChild(liElem);
+    var liElemTxt = document.createTextNode(inputText.value);
+    ulElem.appendChild(liElem);
 
     var divElem = document.createElement("DIV");
     liElem.appendChild(divElem);
-
-    var divElemContainer = document.createElement("DIV");
-    divElem.appendChild(divElemContainer);
-    divElemContainer.appendChild(liElemTxt);
-
-    divElemContainer.setAttribute("class", "text-value");
+    
+    var divElemTwo = document.createElement("DIV");
+    divElem.appendChild(divElemTwo);
+    divElemTwo.appendChild(liElemTxt);
+    divElemTwo.setAttribute("class", "text-value");
 
     liElem.setAttribute("class", "remove");
     divElem.setAttribute("class", "li-flex");
 
-    liElem.getElementsByClassName("remove");
+    liElem.getElementsByClassName("checked");
     liElem.addEventListener("click", function() {
-        var bgColour = this.style.backgroundColor = "#eee";
+        liElem.classList.toggle("checked");
     });
 
-    divElemContainer.getElementsByClassName = "text-value";
-    console.log(divElemContainer.innerText);
+    divElemTwo.getElementsByClassName = "text-value";
+    console.log(divElemTwo.innerText);
     
     var btnElem = document.createElement("BUTTON");
-    btnElem.setAttribute("style", "margin-right: 0;");
-
     var btnElemTxt = document.createTextNode("X");
+    btnElem.setAttribute("style", "margin-right: 0;");
     btnElem.appendChild(btnElemTxt);
     divElem.appendChild(btnElem);
 
@@ -45,13 +44,13 @@ function newTodo() {
 function deleteAllTodos() {
     todoData = [];
 
-    var todoItems = document.getElementById("todo-items");
-    
-    todoItems.remove();
+    var liElem = document.getElementById("todo-items");
+
+    liElem.remove();
 
     var ulElem = document.createElement("UL");
     ulElem.setAttribute("id", "todo-items");
 
-    var controlBar = document.getElementById("main");
-    controlBar.appendChild(ulElem);
+    var inputBlock = document.getElementById("main");
+    inputBlock.appendChild(ulElem);
 }
